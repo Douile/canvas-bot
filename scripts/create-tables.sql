adb52150c189b2c72e54f31d75b0849090fe0353
Douile <25043847+Douile@users.noreply.github.com>
2020-11-09 13:05:05 +0000

    Add database draft

---
+++ scripts/create-tables.sql
@@ -0,0 +1,23 @@
+CREATE TABLE weekly_updates (
+  id INT PRIMARY KEY,
+  message_id STRING,
+  time_start BIGINT,
+  time_end BIGINT
+);
+
+CREATE TABLE assignments (
+  id INT PRIMARY KEY,
+  week_id INT,
+  time_due BIGINT,
+  course_id INT,
+  name STRING,
+  points INT
+);
+
+CREATE TABLE courses (
+  id INT PRIMARY KEY,
+  name STRING,
+  raw_name STRING
+);
+
+
